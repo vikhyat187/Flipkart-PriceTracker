8bee85688d3584528d66165b6c39657e2eafe0e8
vikhyat187 <vikhyatbhatnagarh@gmail.com>
2022-02-20 21:04:10 +0530

    added scripts

---
+++ flipkart.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from smtplib import SMTP
+
+URL = 'https://www.flipkart.com/savya-home-apollo-hbcb-fabric-office-arm-chair/p/itm09573d0715a98?pid=OSCG2WPFZS3NGSZE&otracker=wishlist&lid=LSTOSCG2WPFZS3NGSZEZZWARM&fm=organic&iid=511c4d23-91bf-48be-91b6-6c5c6aab1185.OSCG2WPFZS3NGSZE.PRODUCTSUMMARY&ppt=dynamic&ppn=CART_PAGE&ssid=5p0unlutz40000001645366426570'
+
+SMTP_SERVER ="smtp.gmail.com"
+PORT = 587
+EMAIL_ID = "vikhyatbhatnagarh@gmail.com"
+PASSWORD = "germpgwmtzpxjwce"
+
+
+def extract_price():
+    page = requests.get(URL)
+
+    soup = bs(page.content,"html.parser")
+
+    price_cells = soup.findAll('div',{'class':'_30jeq3 _16Jk6d'})
+
+    for price_cell in price_cells:
+        val = price_cell.text
+
+    numeric_val = val.split('₹')  
+
+    actualvalList = numeric_val[1]
+
+    final_val = int(actualvalList.replace(',',''))
+   
+    if(final_val < 5990):
+        server = SMTP(SMTP_SERVER,PORT)
+        server.starttls()
+        server.login(EMAIL_ID,PASSWORD)
+
+        subject = "Alert for chair"
+        body = "varPrice has fallen go and buy now at " + str(final_val) + "\n" + URL 
+        msg = f"Subject: {subject}\n\n{body}"
+        recipients=['vikhyatbhatnagarh@gmail.com',"santoshbhatnagar81@gmail.com"]
+        server.sendmail(EMAIL_ID,recipients,msg)
+        server.quit()
+
+extract_price()
---
+++ links.md
@@ -0,0 +1,9 @@
+[extracting data from class](https://stackoverflow.com/questions/38546575/how-to-extract-value-from-a-class-in-beautiful-soup)
+
+[split string based on ₹](https://www.geeksforgeeks.org/python-string-split/)
+
+[check type of val](https://www.geeksforgeeks.org/type-isinstance-python/)
+
+[parsing commas ](https://stackoverflow.com/questions/2953746/python-parse-comma-separated-number-into-int)
+
+[send mail to multiple people](https://stackoverflow.com/questions/8856117/how-to-send-email-to-multiple-recipients-using-python-smtplib)
